6a52f7a004d4febe4b2427a72be31696a8b4de64
Connor <disappearing.now@gmail.com>
2024-08-27 20:11:08 -0400

    Adjusted code for Pylint

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+"""
+This module creates the Flask server
+which serves the web app to the user
+that takes text input and returns
+an emotional analysis of the text
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,14 +11,19 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def home():
+    """Renders index.html on the '/' route"""
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def analyze():
+    """
+    Connects to the emotion detector function with 
+    text provided by user and returns appropriate response
+    """
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
-    
-    if response["dominant_emotion"] == None:
+
+    if response["dominant_emotion"] is None:
         return "Invalid text! Please try again!"
 
     anger = f"'anger': {response['anger']}"
